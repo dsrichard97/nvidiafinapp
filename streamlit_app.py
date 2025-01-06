02d1ed842c2d70307aa03078b55144a2021b97fd
Richard DiazDeLeon <Richard.Diazdeleon01@student.csulb.edu>
2025-01-06 00:59:38 +0000

    modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 NVIIDIA FINANCIAL HISTORY")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
